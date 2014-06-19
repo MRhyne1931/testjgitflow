00f95f874a2460c156bc60fcf5742f5f1cc6eeec
Rhyne, Robert <robert_rhyne@premierinc.com>
2014-06-19 10:37:16 -0400

    Committing

---
+++ src/main/java/com/premier/sourcingteam/testjgitflow/App.java
@@ -0,0 +1,13 @@
+package com.premier.sourcingteam.testjgitflow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
